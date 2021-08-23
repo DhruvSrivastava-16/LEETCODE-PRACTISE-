3b5997dff34f9fa7baa0d6f4f3ed95ea69bbd057
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-08-23 15:27:44 -0700

    Time: 52 ms (52.36%), Space: 16.2 MB (65.52%) - LeetHub

--- construct-string-from-binary-tree/construct-string-from-binary-tree.py
+++ construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -6,21 +6,17 @@
 #         self.right = right
 class Solution:
     def help(self,root):
-        if root is None:
+        if root is None: 
             return ''
         
         if root.left is None and root.right is None:
-        
             return str(root.val)
         
         if root.right is None:
-            return str(root.val) + '(' + self.help(root.left)+')'
-        
+            return str(root.val) + '(' + self.help(root.left) + ')'
+            
+        return str(root.val) + '(' + self.help(root.left) + ')' +'('+ self.help(root.right)+')'
     
-        
-        return str(root.val) + '('+self.help(root.left) +')'+'('+ self.help(root.right)+')'
-                                                               
-        
+    
     def tree2str(self, root: Optional[TreeNode]) -> str:
-        a=self.help(root)
-        return(a)
+        return self.help(root)
