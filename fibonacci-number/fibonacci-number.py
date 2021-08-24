834d83b0de759e0aac539f1e057f281867571c24
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-08-23 22:10:12 -0700

    Time: 908 ms (15.43%), Space: 14 MB (89.80%) - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,18 +1,9 @@
 class Solution:
-    def fib(self, N):
-   
-        cache = {}
-        def recur_fib(N):
-            if N in cache:
-                return cache[N]
-
-            if N < 2:
-                result = N
-            else:
-                result = recur_fib(N-1) + recur_fib(N-2)
-
-            # put result in cache for later reference.
-            cache[N] = result
-            return result
-
-        return recur_fib(N)
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        
+        if n == 1:
+            return 1
+        
+        return self.fib(n-1) + self.fib(n-2)
