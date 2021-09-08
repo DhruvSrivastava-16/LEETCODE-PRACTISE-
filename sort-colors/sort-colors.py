627bbc46935df68fe5f6be6f37dac2a64d77c088
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-09-08 11:53:52 -0700

    Time: 36 ms (50.60%), Space: 14.2 MB (75.30%) - LeetHub

---
+++ sort-colors/sort-colors.py
@@ -0,0 +1,40 @@
+class Solution:
+        
+    
+    def mergeSort(self,arr):
+        if len(arr)>1:
+            mid = len(arr)//2
+            L = arr[:mid]
+            R = arr[mid:]
+            self.mergeSort(L)
+            self.mergeSort(R)
+            i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+            while i < len(L) and j < len(R):
+                if L[i] < R[j]:
+                    arr[k] = L[i]
+                    i += 1
+                else:
+                    arr[k] = R[j]
+                    j += 1
+                k += 1
+
+            # Checking if any element was left
+            while i < len(L):
+                arr[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(R):
+                arr[k] = R[j]
+                j += 1
+                k += 1
+
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        self.mergeSort(nums)
+        print(nums)
+        
