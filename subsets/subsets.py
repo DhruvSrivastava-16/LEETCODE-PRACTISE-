86f5b5bbc0eb09e555fcf6462377d47a5f58de0b
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-12 03:46:39 -0700

    Time: 46 ms (27.86%), Space: 14.5 MB (50.33%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,20 +1,23 @@
 class Solution:
-    def generate_subset_len(self,l,temp,nums,pos):
-        if len(temp) == l:
+    def gen_sub_len(self,nums,l,pos,temp):
+        if len(temp)==l:
             self.ans.append(temp[:])
-            return
-        
+            return 
+            
         for i in range(pos,len(nums)):
             temp.append(nums[i])
-            self.generate_subset_len(l,temp,nums,i+1)
+            self.gen_sub_len(nums,l,i+1,temp)
             temp.pop()
-        
-        
+    
     
     def subsets(self, nums: List[int]) -> List[List[int]]:
         self.ans = []
-        for i in range(0,len(nums)+1):
+        
+        for l in range(0,len(nums)+1):
+            temp = []
+            self.gen_sub_len(nums,l,0,temp)
+
             
-            self.generate_subset_len(i,[],nums,0)
-            
-        return(self.ans)
+        return self.ans
+    
+    
