bdb587e47352d7689a78dac2d5580248c372e72a
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-15 16:13:22 -0700

    Time: 105 ms (58.92%), Space: 14.3 MB (99.44%) - LeetHub

---
+++ leftmost-column-with-at-least-a-one/leftmost-column-with-at-least-a-one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        
+        rows, cols = binaryMatrix.dimensions()
+        
+        # Set pointers to the top-right corner.
+        current_row = 0
+        current_col = cols - 1
+        
+        # Repeat the search until it goes off the grid.
+        while current_row < rows and current_col >= 0:
+            if binaryMatrix.get(current_row, current_col) == 0:
+                current_row += 1
+            else:
+                current_col -= 1
+        
+        # If we never left the last column, it must have been all 0's.
+        return current_col + 1 if current_col != cols - 1 else -1
