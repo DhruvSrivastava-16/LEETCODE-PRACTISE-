10f511d2859c4d8c54ab4f8409cf56d31151da2d
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-19 17:50:15 -0700

    Time: 148 ms (5.21%), Space: 15.3 MB (73.97%) - LeetHub

--- convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -8,65 +8,64 @@ class Node:
 """
 
 class Solution:
-    def nav_first(self,root):
+    def left_last_func(self,root):
         if root is None:
-            return 
+            return None
         
+        while root.right.val > root.val:
+            root = root.right
+            
+        return root
+    
+    
+    def right_first_func(self,root):
+        if root is None:
+            return None
+            
         while root.left.val<root.val:
             root = root.left
         
         return root
-        
-    
-    def nav_last(self,root):
-        if root is None:
-            return 
-        
-        while root.right.val>root.val:
-            root = root.right
-        
-        return root
-        
+            
     
     def inorder(self,root):
         if root is None:
-            return 
+            return None
         
         self.inorder(root.left)
-        #reached the left most node
-        left_last_node = self.nav_last(root.left)
-        root.left = left_last_node
         
-        if left_last_node is not None:
-            left_last_node.right = root
+        left_last = self.left_last_func(root.left)
+        root.left  = left_last
         
-        #self.root.right = parent
+        if left_last is not None:
+            left_last.right = root
+        
         self.inorder(root.right)
-        right_first_node = self.nav_first(root.right)
-        root.right = right_first_node
+        right_first = self.right_first_func(root.right)
         
-        if right_first_node is not None:
-            right_first_node.left = root
+        if right_first is not None:
+            right_first.left = root
         
-        head = self.nav_first(root.left)
-        tail = self.nav_last(root.right)
+        root.right = right_first
+        
+        head = self.right_first_func(root.left)
+        tail = self.left_last_func(root.right)
         
         if head is None:
             head = root
+            
         
         if tail is None:
-            tail = root    
+            tail = root
+            
+        head.left = tail
+        
             
         tail.right = head
-        head.left = tail
         
         return head
         
-        
-        
-        
-        
     
     def treeToDoublyList(self, root: 'Node') -> 'Node':
-        root = self.inorder(root)
-        return root
+        return self.inorder(root)
+        
