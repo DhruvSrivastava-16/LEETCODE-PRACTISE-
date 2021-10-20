ee7667d8e2697d73cd01c0706463204f0989512e
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-20 15:07:53 -0700

    Time: 40 ms (71.47%), Space: 14.5 MB (46.93%) - LeetHub

---
+++ word-break/word-break.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        q = deque()
+        visited = set()
+
+        q.append(0)
+        while q:
+            start = q.popleft()
+            if start in visited:
+                continue
+            
+            visited.add(start)
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in word_set:
+                    q.append(end)
+                    if end == len(s):
+                        return True
+                
+        return False
