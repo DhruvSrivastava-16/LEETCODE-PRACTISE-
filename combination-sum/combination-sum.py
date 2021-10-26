f8e02fe62edb6d9569b4fe81d9ebc7dc48fb2c5a
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-26 00:13:24 -0700

    Time: 92 ms (59.50%), Space: 14.2 MB (77.23%) - LeetHub

--- combination-sum/combination-sum.py
+++ combination-sum/combination-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     ans = []
     def helper(self,candidates,target,s,temp,start):
-        if target<0:
+        if sum(temp)>target:
             return
         
-        elif target==0:
+        elif sum(temp)==target:
             t = tuple(temp)
             
             self.ans.append(temp[:])
@@ -13,7 +13,7 @@ class Solution:
             for j in range(start,len(candidates)):
                 temp.append(candidates[j])
                 #print(temp)
-                self.helper(candidates,target-candidates[j],s,temp,j)
+                self.helper(candidates,target,s,temp,j)
                 temp.pop()
                 
         
