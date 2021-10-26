0e70d74f79ae2fc22d8eb57c46b3127a3c677eb6
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-26 00:52:08 -0700

    Time: 52 ms (74.05%), Space: 14.6 MB (48.51%) - LeetHub

--- next-greater-element-i/next-greater-element-i.py
+++ next-greater-element-i/next-greater-element-i.py
@@ -1,34 +1,46 @@
+from collections import defaultdict
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hmap = {}
-        mst  = []
-        
-        for i in range(0,len(nums2)):
-            hmap[nums2[i]] = i
-        
-        for j in nums2:
-            if len(mst)<=0:
-                mst.append(j)
-                
-            elif mst[-1]<j:
-                    
-                    while len(mst)>0 and mst[-1]<j:
-                        print('MST:',mst)
-                        temp = mst.pop()
-                        hmap[temp] = j
-            
-            mst.append(j)
-                        
-                        
-        print(hmap)
-        print(mst)
-        while len(mst)>0:
-            temp = mst.pop()
-            hmap[temp] = -1
-                    
+        h_map = defaultdict(int)
+        stk = []
         ans = []
-        print(hmap)
-        for i in nums1:
-            ans.append(hmap[i])
+        for i in range(len(nums2)-1,-1,-1):
             
-        return(ans)
+            if not stk:
+                
+                stk.append(nums2[i])
+                ans.append(-1)
+                h_map[nums2[i]]=ans[-1]
+                
+            else:
+                if stk[-1]>nums2[i]:
+                    
+                    ans.append(stk[-1])
+                    
+                    stk.append(nums2[i])
+                    h_map[nums2[i]]=ans[-1]
+                    
+                else:
+                    while stk and stk[-1]<nums2[i]:
+                        stk.pop()
+                    
+                    if not stk:
+                        ans.append(-1)
+                        
+                    else:
+                        ans.append(stk[-1])
+                        
+                    stk.append(nums2[i])
+                    h_map[nums2[i]]=ans[-1]
+                    
+        print(stk,ans,h_map)
+        f = []
+        for j in nums1:
+            f.append(h_map[j])
+        return f
+            
+                    
+                
+                
+            
+            
