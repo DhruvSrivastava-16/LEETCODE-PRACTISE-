34e7dfdf533f0ab3dda0a708fc8212d9751fdd11
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-26 21:18:41 -0700

    Time: 400 ms (94.55%), Space: 15.4 MB (87.39%) - LeetHub

---
+++ koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, H):
+        # Can Koko eat all bananas in H hours with eating speed K?
+        def possible(K):
+            return sum((p-1) // K + 1 for p in piles) <= H
+
+        lo, hi = 1, max(piles)
+        while lo < hi:
+            mi = (lo + hi) // 2
+            if not possible(mi):
+                lo = mi + 1
+            else:
+                hi = mi
+        return lo
