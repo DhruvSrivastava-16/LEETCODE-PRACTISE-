9cac59ccc3f48dbf68e8bd3dfc2113d2051195a0
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-28 00:22:40 -0700

    Time: 100 ms (45.24%), Space: 15.9 MB (97.60%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,24 +1,21 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key = lambda x:x[0])
+        
         merged = []
+        intervals.sort(key = lambda x:x[0])
         
         for interval in intervals:
-            
-            if len(merged)==0:
-                merged.append(interval)
-                continue
+            print(merged)
+            if merged:
+                print('y')
+                if merged[-1][0]<=interval[0] and merged[-1][1]>=interval[0]:
+                    merged[-1][1] = interval[1] if interval[1]> merged[-1][1] else merged[-1][1]
                 
-            if merged[-1][0]<=interval[0] and merged[-1][1]>=interval[0]:
-                merged[-1][1] = interval[1] if interval[1]> merged[-1][1] else merged[-1][1]
-            
+                else:
+                    merged.append(interval)
+                
             else:
+                print('em')
                 merged.append(interval)
                 
-            #return(merged)
-                
-        return(merged)
-        
-                
-            
-        
+        return merged
