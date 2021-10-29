34381b99281f7125704e9c52b98d553fd84e3623
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-29 00:52:34 -0700

    Time: 32 ms (82.39%), Space: 14.5 MB (17.37%) - LeetHub

--- one-edit-distance/one-edit-distance.py
+++ one-edit-distance/one-edit-distance.py
@@ -1,28 +1,27 @@
 class Solution:
     def isOneEditDistance(self, s: str, t: str) -> bool:
+        
         sz_s = len(s)
         sz_t = len(t)
         
         if abs(sz_s-sz_t)>1:
             return False
         
-        if sz_s>sz_t:
-            smaller = t
-            bigger = s
-            
-        else:
+        if sz_s<sz_t:
             smaller = s
             bigger = t
             
-        print(smaller,bigger)
+        else:
+            smaller = t
+            bigger = s
             
-        for i in range(len(smaller)):
-            print(i)
+        
+        for i in range(0,len(smaller)):
             if smaller[i]!=bigger[i]:
-                if len(bigger) == len(smaller):
-                    return smaller[i+1:]==bigger[i+1:]
-                
-                else:
+                if sz_s!=sz_t:
                     return smaller[i:]==bigger[i+1:]
                 
-        return len(smaller)+1==len(bigger)
+                else:
+                    return smaller[i+1:]==bigger[i+1:]
+                
+        return abs(sz_s-sz_t)==1
