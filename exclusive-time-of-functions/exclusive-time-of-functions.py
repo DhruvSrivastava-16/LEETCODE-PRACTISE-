cd12e168518c99a81f3bab84a2d9d200527b3004
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-10-28 17:33:25 -0700

    Time: 92 ms (39.51%), Space: 14.4 MB (55.84%) - LeetHub

--- exclusive-time-of-functions/exclusive-time-of-functions.py
+++ exclusive-time-of-functions/exclusive-time-of-functions.py
@@ -1,41 +1,36 @@
-from collections import defaultdict
-
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        
         h_map = defaultdict(int)
-        p_stack = []
+        stk = []
         
         for log in logs:
-            p_id, mode, time = log.split(':')
-            time=int(time)
-            #print(type(p_id), mode, type(time))
+            p_id, op, time = log.split(':')
+            time = int(time)
+            print(type(p_id))
             
-            if mode=='start':
                 
-                if not p_stack:
-                    p_stack.append([p_id,time])
+           
+            if op=='start':
+                if not stk:
+                    stk.append([p_id,time])
+                    continue
                     
-                else:
-                    h_map[p_stack[-1][0]]+=time-p_stack[-1][1]
-                    print("UP; Adding time to",p_stack[-1][0],time-p_stack[-1][1])
-                    p_stack.append([p_id,time])
-                    
-            else:
-                print(p_stack)
-                p_pid, end_t = p_stack.pop()
-                if len(p_stack)!=0:
-                    print('time updated',time)
-                    p_stack[-1][1]=time+1
-                print('p_stack after pop:',p_stack)
-                print('ending:',p_pid,end_t,time)
-                print("PP; Adding time to",p_pid,time-end_t+1)
+                print('--',type(stk[-1][0]))
+                h_map[stk[-1][0]] += time - stk[-1][1]
+                stk[-1][1] = int(time)
+                stk.append([p_id,time])
 
-                h_map[p_pid]+=time-end_t+1
-               
-        print(h_map)
-        answer = []
-        for k,v in h_map.items():
-            answer.append(v)
+            else:
+                print('-',type(p_id))
+                h_map[p_id] += time - stk[-1][1]+1
+                stk.pop()
+                if stk:
+                    stk[-1][1] = time+1
+                    
+        return(h_map.values())
+                    
+                    
+                    
             
-        return answer
         
