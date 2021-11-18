f94d33a5fcaff628c1ebfb98ac2d2d3084184db4
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-11-17 23:58:53 -0800

    Time: 7340 ms (28.56%), Space: 14.2 MB (91.10%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,28 +1,34 @@
 class Solution:
+    
+    def backtrack(self,board,word,i,j):
+        if len(word)==0:
+            return True
+        
+        if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or board[i][j]!=word[0]:
+            return False
+        
+        board[i][j] = '#'
+        
+        for k in [(-1,0),(0,1),(1,0),(0,-1)]:
+            
+            if self.backtrack(board,word[1:],i+k[0],j+k[1]):
+                return True
+            
+        board[i][j] = word[0]
+        
+        return False
+            
+            
+        
+        
+        
+    
+    
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        for i in range(0,len(board)):
-            for j in range(0,len(board[0])):
-                if self.backtrack(i,j,word,board):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.backtrack(board,word,i,j):
                     return True
                 
-        return False
-    
-    
-    def backtrack(self,i,j,suffix,board):
-        if len(suffix) == 0:
-            return True
-
-        if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or board[i][j]!=suffix[0]:
-            return False
-
-        board[i][j] = '#'
-
-        for rowOffset, colOffset in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
-
-            if self.backtrack(i+rowOffset,j+colOffset,suffix[1:],board):
-                return True
-
-        board[i][j] = suffix[0]
-
         return False
