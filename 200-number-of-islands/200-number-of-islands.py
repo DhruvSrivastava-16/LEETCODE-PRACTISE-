6ca8d60acee50a828982beee3286fe5587d96344
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-06-02 23:01:55 -0700

    Time: 371 ms (66.51%), Space: 16.3 MB (90.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,12 +4,12 @@ dy = [0,1,0,-1]
 
 class Solution:
     
-    def isValid(self, row, col, mat, vis):
+    def isValid(self, row, col, mat):
         
         if row<0 or col<0 or row>=len(mat) or col>=len(mat[0]):
             return False
         
-        if vis[row][col] or mat[row][col]=='0':
+        if mat[row][col]=='0':
             return False
         
         return True
@@ -27,10 +27,10 @@ class Solution:
         for i in range(0,len(grid)):
             for j in range(0,len(grid[0])):
                 
-                if grid[i][j] == '1' and not vis[i][j]:
+                if grid[i][j] == '1':
                     dq.append([i,j])
                     num+=1
-                    vis[i][j] = True
+                
                     
         
                     while dq:
@@ -42,10 +42,10 @@ class Solution:
                             eltx = elx + dx[itr]
                             elty = ely + dy[itr]
 
-                            if self.isValid(eltx, elty, grid, vis):
+                            if self.isValid(eltx, elty, grid):
 
                                 dq.append([eltx, elty])
-                                vis[eltx][elty] == True
+                                # vis[eltx][elty] == True
                                 grid[eltx][elty] = '0'
                     
                     
