2b89e0c45e94c88ae2c1e00570e166c006042e4b
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-06-09 23:17:42 -0700

    Time: 77 ms (13.33%), Space: 14 MB (57.78%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,35 @@
+class Solution:
+    
+    def backtracking(self, nums, temp, i,ans,vis):
+        vis.add(i)
+
+        if len(temp)==len(nums):
+            ans.append(temp[:])
+            return 
+        
+        
+        for j in range(0,len(nums)):
+            if j not in vis:
+                temp.append(nums[j])
+                self.backtracking(nums,temp,j,ans,vis)
+                temp.pop()
+                vis.remove(j)
+            
+            
+            
+            
+        return
+        
+        
+        
+    
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        for i in range(0,len(nums)):
+            temp = []
+            vis = set()
+            temp.append(nums[i])
+            print(i,temp)
+            self.backtracking(nums,temp,i,ans,vis)
+            
+        return ans
