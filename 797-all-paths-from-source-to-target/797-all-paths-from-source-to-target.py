d1aa1a9e4646fac272f762ee2ae6d6d0891fbec8
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-06-12 20:16:59 -0700

    Time: 183 ms (15.54%), Space: 16 MB (13.88%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def backtrack(self, graph, path, paths, ne):
+        path.append(ne)
+        print("Ne",ne,len(graph)-1)
+
+        if ne == len(graph)-1:
+            paths.append(path[:])
+            return
+            
+        for neg in graph[ne]:
+            self.backtrack(graph,path,paths,neg)
+            path.pop()
+            
+    
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        paths = []
+        path = [0]
+        
+        for ne in graph[0]:
+            self.backtrack(graph,path,paths,ne)
+            path = [0]
+        
+        return(paths)
