b5273487a7cf422ff836146acc1a6a6f16c87db9
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-06-13 22:44:10 -0700

    Time: 634 ms (10.78%), Space: 29.6 MB (5.11%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+class Solution:
+    
+    def dfs(self, graph, k, v, CC, visited):
+        CC.append(k)
+        visited.add(k)
+        for n in graph[k]:
+            if n not in visited:
+                self.dfs(graph, n, v, CC, visited)
+        
+        
+        
+    
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        graph = defaultdict(set)
+        nameMap = defaultdict(set)
+        
+        for account in accounts:
+            for itr in range(1,len(account)):
+                tempSet = set(account[1:])
+                tempSet.remove(account[itr])
+                graph[account[itr]]=graph[account[itr]].union(tempSet)
+                nameMap[account[itr]] = account[0]
+        
+        visited = set()
+        CC = []
+        answer = []
+        for k,v in graph.items():
+            CC = []
+            if k not in visited:
+
+                self.dfs( graph, k, v, CC, visited)
+
+                answer.append(CC[:])
+                
+        print(answer,visited)
+        
+        final_answer = []
+        temp = []
+
+        for ans in answer:
+            temp = []
+            temp.append(nameMap[ans[0]])
+            ans.sort()
+            temp += ans[0:]
+            final_answer.append(temp[:])
+
+        return(final_answer)
+        
+            
+            
