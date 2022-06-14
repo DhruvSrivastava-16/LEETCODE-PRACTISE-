425d8d5ab40b133851661b460a723cae40ffdcac
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-06-14 16:27:12 -0700

    Time: 72 ms (5.97%), Space: 13.8 MB (71.02%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,32 +1,17 @@
 class Solution:
-    
-    def recursion(self,arr,nums,high):
-        if int(nums)>high:
-            return 
-        
-        if int(nums) not in arr:
-            arr.add(int(nums))
-            
-        if nums[-1]==str(9):
-            nums = ''.join([str(i) for i in range(1,len(nums)+2)])
-            arr.add(int(nums))
-        
-        for j in range(int(nums[-1])+1,10):
-            nums += str(j)
-            self.recursion(arr,nums,high)
-            
-
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        arr = set()
-        for i in range(1,10):
-            temp = str(i)
-            self.recursion(arr,temp,high)
         
-        fans = []
-        for j in arr:
-            if j>=low and j<=high:
-                fans.append(j)
+        sample = "123456789"
+        ans = []
+        n = 10
         
-        fans.sort()
-        
-        return list(fans)
+        for length in range(len(str(low)),len(str(high))+1):
+            
+            for st in range(n-length):
+                num = int(sample[st:st+length])
+                if num >= low and num<=high:
+                    ans.append(num)
+                    
+                    
+                    
+        return ans
