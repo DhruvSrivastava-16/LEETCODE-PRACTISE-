1b249b6faa7904fb28c05ae31e2d2943c5cf5e02
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-06-13 23:51:52 -0700

    Time: 129 ms (26.03%), Space: 14 MB (65.07%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,24 @@
+class Solution:
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
+    
+    def helper(self, sr, sc, image, newColor, nColor,visited):
+        if sr<0 or sr>=len(image) or sc<0 or sc>=len(image[0]) or (sr, sc) in visited:
+            return 
+        visited.add((sr,sc))
+        if image[sr][sc]==nColor:
+            image[sr][sc] = newColor
+
+        
+            self.helper(sr+1,sc,image,newColor,nColor,visited)
+            self.helper(sr,sc+1,image,newColor,nColor,visited)
+            self.helper(sr,sc-1,image,newColor,nColor,visited)
+            self.helper(sr-1,sc,image,newColor,nColor,visited)
+    
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        
+        visited = set()
+        self.helper(sr, sc, image, newColor,image[sr][sc],visited)
+        return image
+        
+        
