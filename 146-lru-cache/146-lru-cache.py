b266d9827aa4f45e94710cbc09effe2221614807
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-06-16 16:06:50 -0700

    Time: 1685 ms (14.61%), Space: 74.5 MB (95.57%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.py
@@ -0,0 +1,39 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity 
+        self.hmap = {}
+
+    def get(self, key: int) -> int:
+        if key in self.hmap:
+            val = self.hmap.pop(key)
+            self.put(key,val)
+            
+            return val
+        
+        else:
+            return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        
+        if key in self.hmap:
+            valueOld = self.hmap.pop(key)
+            self.put(key,value)
+            
+        elif len(self.hmap) == self.capacity:
+            remove = list(self.hmap.keys())[0]
+            self.hmap.pop(remove)
+            self.hmap[key] = value
+        
+        else:
+            self.hmap[key] = value
+            
+            
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
