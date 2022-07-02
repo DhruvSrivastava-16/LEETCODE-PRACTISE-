35c34b007553500914fbfaf382c528d04af420ae
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-02 12:04:40 -0700

    Time: 87 ms (5.31%), Space: 14 MB (49.64%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lcp(self,string1,string2):
+        itr1, itr2 = 0, 0
+        common = ''
+        
+        while itr1<len(string1) and itr2<len(string2):
+            if string1[itr1]==string2[itr2]:
+                common+=string1[itr1]
+                
+            else:
+                return common
+            
+            itr1+=1
+            itr2+=1
+        
+        return common
+    
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        if len(strs)==1:
+            return strs[0]
+        
+        cp = self.lcp(strs[0],strs[1])
+        if len(strs)==2:
+            return cp
+        
+        for i in range(2,len(strs)):
+            cp = self.lcp(cp,strs[i])
+            
+        return cp
