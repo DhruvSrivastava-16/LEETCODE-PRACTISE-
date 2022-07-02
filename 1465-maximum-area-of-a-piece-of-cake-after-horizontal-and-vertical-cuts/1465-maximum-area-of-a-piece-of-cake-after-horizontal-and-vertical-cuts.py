fc56fea689c3c136b35c70b6f4856899e173eaf8
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-02 12:40:31 -0700

    Time: 335 ms (93.61%), Space: 26.9 MB (50.43%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -3,13 +3,11 @@ class Solution:
     def findMaxCont(self,arr,st,end):
         
         if len(arr)==1:
-            print('returning:',max(arr[0],end-arr[0]))
             return max(arr[0],end-arr[0])
         
         maxx = max(arr[0],end-arr[-1])
     
         for i in range(1,len(arr)):
-            
             maxx = max(maxx,arr[i]-arr[i-1])
                 
         return maxx
