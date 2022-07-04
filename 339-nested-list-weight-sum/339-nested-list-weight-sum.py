9c76971ba66a6a082f9f931c39777b8b2e296c4f
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-04 14:49:49 -0700

    Time: 61 ms (17.47%), Space: 14.1 MB (47.89%) - LeetHub

--- 339-nested-list-weight-sum/339-nested-list-weight-sum.py
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.py
@@ -42,14 +42,23 @@
 #        """
 
 class Solution:
+    
+    def dfs(self,nl,depth):
+        total = 0
+        for nested in nl:
+            if nested.isInteger():
+                total += nested.getInteger()*depth
+                
+            else:
+                total += self.dfs(nested.getList(),depth+1)
+                
+        return total
+    
     def depthSum(self, nestedList: List[NestedInteger]) -> int:
-        def dfs(nested_list, depth):
-            total = 0
-            for nested in nested_list:
-                if nested.isInteger():
-                    total += nested.getInteger() * depth
-                else:
-                    total += dfs(nested.getList(), depth + 1)
-            return total
-
-        return dfs(nestedList, 1)
+        
+        total = self.dfs(nestedList,1)
+        return total
+    
+ 
+    
+    
