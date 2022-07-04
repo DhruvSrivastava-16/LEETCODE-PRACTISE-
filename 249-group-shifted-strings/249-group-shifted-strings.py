32b1b0f73458797eeedcd6eb1bf87083372a0ad1
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-04 15:33:16 -0700

    Time: 40 ms (90.16%), Space: 13.9 MB (44.36%) - LeetHub

---
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        codeMap = defaultdict(list)
+        for st in strings:
+            code = ''
+            for itr in range(len(st)):
+                
+                code+=chr((ord(st[itr]) - ord(st[0])) % 26 + ord('a'))
+                
+            codeMap[code].append(st)
+            
+        return list(codeMap.values())
+                
+            
+                
+                
