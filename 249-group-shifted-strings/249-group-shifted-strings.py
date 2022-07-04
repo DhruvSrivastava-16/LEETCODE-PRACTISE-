6969afd8d3cb82d1d0b07a358035b0eebbc137db
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-04 15:36:43 -0700

    Time: 74 ms (15.88%), Space: 13.9 MB (85.59%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -5,10 +5,10 @@ class Solution:
             code = ''
             for itr in range(len(st)):
                 
-                code+=str((ord(st[itr]) - ord(st[0])) % 26 + ord('a'))
+                code+=str((ord(st[itr]) - ord(st[0])) % 26 + 100)
                 
             codeMap[code].append(st)
-            
+        print(codeMap)
         return list(codeMap.values())
                 
             
