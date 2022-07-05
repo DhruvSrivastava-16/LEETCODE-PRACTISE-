80b419727fef805c3047431a3257f84db1c6eabf
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-04 19:02:58 -0700

    Time: 370 ms (58.05%), Space: 21.7 MB (42.32%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.py
@@ -0,0 +1,32 @@
+class FileSystem:
+
+    def __init__(self):
+        self.map = defaultdict(list)
+
+    def createPath(self, path: str, value: int) -> bool:
+        temp = path.split('/')
+        print("P:",temp)
+        if len(temp)==2 and path not in self.map:
+            self.map[path] = value
+            return True
+            
+        parent = '/'.join(temp[:-1])
+        if parent not in self.map:
+            return False
+        
+        if path not in self.map:
+            self.map[path] = value
+            return True
+        
+        return False
+
+    def get(self, path: str) -> int:
+        if path not in self.map:
+            return -1
+        return self.map[path]
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
