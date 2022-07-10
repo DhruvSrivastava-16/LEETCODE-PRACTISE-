58114743b6a487b3e7c63e28ad60ed04dabd89d8
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-10 13:37:20 -0700

    Time: 251 ms (61.60%), Space: 14.5 MB (49.32%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -8,19 +8,16 @@ class Solution:
         colorMap = dict()
         
         for i in range(len(graph)):
-            print(i)
             if i not in colorMap:
                 
                 dq.append((i,1))
                 colorMap[i]=1
                 
                 while dq:
-                    print(dq)
                     node, color = dq.popleft()
                     colorMap[node] = color
                     
                     for n in graph[node]:
-                        print('ne:',n)
                         if n not in colorMap:
                             
                             dq.append((n,-color))
