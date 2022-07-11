0ab799d19735f86164c9c952baef2a8a8e538e79
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-11 00:36:35 -0700

    Time: 162 ms (76.68%), Space: 15.2 MB (33.21%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
