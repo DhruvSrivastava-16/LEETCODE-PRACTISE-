8c906b7ecbfced60e83edd8e2a4c803c5feae4ad
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-12 19:09:17 -0700

    Time: 103 ms (97.67%), Space: 14.5 MB (11.73%) - LeetHub

---
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.py
@@ -0,0 +1,69 @@
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, row: int, col: int) -> int:
+#    def dimensions(self) -> list[]:
+
+class Solution:
+    
+    def binSearch(self,row,left,right,binaryMatrix):
+        
+        Found = False
+        
+        while left<=right:
+            mid = (left+right)//2
+
+            if binaryMatrix.get(row,mid)==1:
+                Found = True
+                loc = mid
+                right = mid-1
+                
+            else:
+                left = mid+1
+                
+        if Found:
+            return loc
+        
+        return -1
+
+        
+        
+    
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        r, c = binaryMatrix.dimensions()
+        left = 0
+        right = c-1
+        rowN = 0
+        pos = -1
+        itr = 0
+        answer = []
+        
+        while itr<r:
+            print(itr,pos)
+            if itr==0:
+                pos = self.binSearch(itr,left,right,binaryMatrix)
+                
+            elif pos==-1:
+                pos = self.binSearch(itr,left,right,binaryMatrix)
+                
+            elif pos>=0 and pos<c and binaryMatrix.get(itr,pos)==1:
+                right = pos-1
+                pos = self.binSearch(itr,left,right,binaryMatrix)
+            
+            if pos>=0:
+                answer.append(pos)
+            print('Got:',pos)
+            itr+=1
+        
+        if not answer:
+            return -1
+        print(answer)
+        return min(answer)
+                
+                
+            
+            
+            
+        
