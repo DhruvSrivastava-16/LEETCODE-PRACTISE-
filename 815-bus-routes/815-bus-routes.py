541b37bbbe485138419656d2f75d02cceaf0dd49
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-12 21:55:01 -0700

    Time: 1214 ms (21.74%), Space: 34.6 MB (64.21%) - LeetHub

---
+++ 815-bus-routes/815-bus-routes.py
@@ -0,0 +1,61 @@
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        StopBusMap = defaultdict(list)
+        
+        if source==target:
+            return 0
+        
+        for i in range(len(routes)):
+            for j in routes[i]:
+                StopBusMap[j].append(i)
+        
+        print(StopBusMap)
+        
+        graph = defaultdict(set)
+        
+        for k,v in StopBusMap.items():
+            
+            for b in v:
+                    
+                for i in v:
+                    
+                    if b!=i:
+                        graph[b].add(i)
+                
+        print(graph)
+        answer = float('inf')
+        
+        for node in StopBusMap[source]:
+            print('N:',node)
+            dq = deque()
+            dist = 1
+            visit = set()
+            
+            dq.append((node,dist))
+            visit.add(node)
+            
+            while dq:
+                
+                node, dist = dq.pop()
+                if node in StopBusMap[target]:
+                    answer = min(dist,answer)
+                
+                for ne in graph[node]:
+                    
+                    if ne in StopBusMap[target]:
+                        answer = min(answer,dist+1)
+                        
+                    if ne not in visit:
+                        visit.add(ne)
+                        dq.append((ne,dist+1))
+                        
+        if answer==float('inf'):
+            return -1
+        
+        return answer
+            
+            
+        
+            
+            
+            
