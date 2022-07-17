f9bbccc15b2c98911de515e33a9aad70d7655e1b
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-17 15:11:49 -0700

    Time: 1096 ms (41.28%), Space: 22 MB (32.79%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.py
@@ -0,0 +1,46 @@
+def retFalse():
+    return False
+
+class Solution:
+
+    
+    def dfs(self,graph,node,visited):
+        
+        visited.add(node)
+        self.stk[node]=True
+
+        
+        for ne in graph[node]:
+            
+            if ne not in visited:
+                if self.dfs(graph,ne,visited):
+                    return True
+                
+            elif self.stk[ne]==True:
+                self.cycle = True
+                return True
+            
+                
+        self.stk[node]=False
+        return False
+        
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        
+        nodes = len(graph)
+        visited = set()
+        self.stk = defaultdict(retFalse)
+        self.cycle = False
+        answer = []
+        self.globalVisited = set()
+        
+        for n in range(nodes):
+
+            if not self.dfs(graph,n,visited):
+                    answer.append(n)
+
+            
+                
+        return answer
+            
+            
+            
