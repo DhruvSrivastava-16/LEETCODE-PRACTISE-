b64c93fef9245b8d36d04a142563eeaff3bfc5b9
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-18 20:43:41 -0700

    Time: 80 ms (17.99%), Space: 14 MB (71.00%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,23 +2,25 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
         dq = deque()
-        word_set = set(wordDict)
-        dq.append(0)
         visited = set()
+        
+        dq.append(0)
         visited.add(0)
+        wordS = set(wordDict)
         
         while dq:
+            
             
             st = dq.popleft()
             
             for i in range(st+1,len(s)+1):
-                if s[st:i] in word_set:
+                
+                if s[st:i] in wordS:
                     if i not in visited:
                         visited.add(i)
                         dq.append(i)
-                        
-                    if i == len(s):
+                    
+                    if i==len(s):
                         return True
                     
         return False
-            
