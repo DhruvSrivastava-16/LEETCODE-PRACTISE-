c8241eac9820edcd66200948ed2ddd381be96393
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-22 10:39:53 -0700

    Time: 67 ms (47.11%), Space: 14 MB (24.65%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,54 @@
+class Solution:
+    
+    def revArr(self,nums,left,right):
+        
+
+        
+        while left<=right:
+            
+            nums[left], nums[right] = nums[right], nums[left]
+            left+=1
+            right-=1
+            
+    
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        if len(nums)==1:
+            return nums
+        
+        Found = False
+        
+        for i in range(len(nums)-1,0,-1):
+            if nums[i-1]<nums[i]:
+                Found = True
+                break
+                
+        smaller = nums[i-1]
+        smallerPos = i-1
+                
+        if not Found:
+            self.revArr(nums,0,len(nums)-1)
+            return 
+        
+        curr = float('inf')
+        currPos = 0
+        
+        for i in range(smallerPos+1,len(nums)):
+            
+            if nums[i]>smaller and nums[i]<=curr:
+                curr = nums[i]
+                currPos = i
+         
+        print(nums,curr)
+        nums[currPos], nums[smallerPos] = nums[smallerPos], nums[currPos]
+        print(nums)
+        
+        self.revArr(nums,smallerPos+1,len(nums)-1)
+        
+        return nums
+                
+        
+                
