2cfb9bacf7ad041090c292e997d21ec77a8733c6
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-22 11:07:27 -0700

    Time: 58 ms (96.05%), Space: 14.3 MB (59.35%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    
+    def bt(self,nums,temp,answer,numsC,keys):
+        # print(nums,temp,answer)
+        if len(temp)==len(nums):
+            answer.append(temp[:])
+            return 
+        
+        for i in keys:
+            if numsC[i]!=0:
+                numsC[i]-=1
+                temp.append(i)
+                self.bt(nums,temp,answer,numsC,keys)
+                numsC[i]+=1
+                temp.pop()
+            
+                
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        answer = []
+        numsC = Counter(nums)
+        keys = numsC.keys()
+        temp = []
+
+        self.bt(nums,temp,answer,numsC,keys)
+            
+        return(answer)
