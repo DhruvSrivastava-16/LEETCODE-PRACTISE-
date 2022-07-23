568f7a594f1fd4e1d2b03f7550e63f690e7145f5
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-23 11:30:19 -0700

    Time: 351 ms (22.85%), Space: 31.5 MB (65.43%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,36 +1,63 @@
 class TrieNode:
+    
     def __init__(self):
-        self.isEnd = False
         self.children = {}
-
+        self.isEnd = False
 
 class Trie:
 
     def __init__(self):
         self.root = TrieNode()
+        
 
     def insert(self, word: str) -> None:
-        curr_node = self.root
-        for letter in word:
-            if letter not in curr_node.children:
-                curr_node.children[letter] = TrieNode()
-            curr_node = curr_node.children[letter]
-        curr_node.isEnd = True
-
+        
+        curr = self.root
+        
+        for char in word:
+            
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+                
+            curr = curr.children[char]
+                
+                
+        curr.isEnd = True
+        
 
     def search(self, word: str) -> bool:
-        curr_node = self.root
-        for letter in word:
-            if letter not in curr_node.children:
+        
+        curr = self.root
+        
+        for char in word:
+            
+            if char not in curr.children:
                 return False
-            curr_node = curr_node.children[letter]
-        return curr_node.isEnd
-
+            
+            curr = curr.children[char]
+                
+        
+        return curr.isEnd
 
     def startsWith(self, prefix: str) -> bool:
-        curr_node = self.root
-        for letter in prefix:
-            if letter not in curr_node.children:
+        
+        curr = self.root
+        
+        for char in prefix:
+            
+            if char not in curr.children:
                 return False
-            curr_node = curr_node.children[letter]
+            
+
+            curr = curr.children[char]
+                
+                
         return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
