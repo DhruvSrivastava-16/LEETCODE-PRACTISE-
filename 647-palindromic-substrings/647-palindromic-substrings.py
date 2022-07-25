aa9402ab1b968433e1e4e4f0835b601bf908f7d0
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-24 21:41:46 -0700

    Time: 901 ms (13.47%), Space: 22.7 MB (10.45%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -4,26 +4,24 @@ class Solution:
         dp = [[False for i in range(len(s))] for j in range(len(s))]
         ans = 0
         
-        for i in range(len(s)):
+        for i in range(0,len(s)):
             dp[i][i] = True
             
-        for i in range(len(s)-1):
-            dp[i][i+1] = s[i]==s[i+1]
+        for i in range(0,len(s)-1):
+            dp[i][i+1] = s[i] == s[i+1]
             
         for length in range(3,len(s)+1):
             st = 0
-            end = length+st-1
+            end = st+length-1
+            
             while end<len(s):
-                dp[st][end] = dp[st+1][end-1] and (s[st]==s[end])
-
-                end+=1
+                dp[st][end] = dp[st+1][end-1] and s[st]==s[end]
                 st+=1
-        
+                end+=1
+                
         for i in range(len(s)):
             for j in range(len(s)):
                 if dp[i][j]:
                     ans+=1
                     
         return ans
-                
-                
