9c43c758891b63e3cdd704fbf5eda51206154da5
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-26 18:31:48 -0700

    Time: 32 ms (96.71%), Space: 14.1 MB (81.58%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,20 +1,22 @@
 class Solution:
-    def BT(self, sz, temp, pos,ans,nums):
+    
+    def helper(self,nums,temp,store,sz,pos):
         if sz==0:
-            ans.append(temp[:])
-            return
+            store.append(temp[:])
+            return 
         
-        for j in range(pos, len(nums)):
-            temp.append(nums[j])
-            self.BT(sz-1, temp, j+1,ans,nums)
+        for i in range(pos,len(nums)):
+            temp.append(nums[i])
+            self.helper(nums,temp,store,sz-1,i+1)
             temp.pop()
-            
         
-            
+    
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        for i in range(0,len(nums)+1):
-            temp = []
-            self.BT(i,temp,0,ans,nums)
-
-        return ans
+        
+        temp = []
+        store = []
+        
+        for sz in range(0,len(nums)+1):
+            self.helper(nums,temp,store,sz,0)
+            
+        return (store)
