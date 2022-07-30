df384fea27d9ae6c30743372aba55d7ddaf19244
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-29 22:49:37 -0700

    Time: 8830 ms (5.11%), Space: 28.8 MB (27.49%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -0,0 +1,39 @@
+class Solution:
+    
+    def isPoss(self,mid,time,totalTrips):
+        total = 0
+        # print('M:',mid,time)
+        for i in range(len(time)):
+            total += int(mid/time[i])
+        # print('T:',total)
+        return total>=totalTrips
+            
+            
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        
+        total = 0
+        currTotal = 0
+        clock = 0
+        time.sort()
+        
+        right = time[-1]*totalTrips
+        left = 0
+        ans = []
+        
+        while left<=right:
+            
+            mid = (left+right)//2
+            if self.isPoss(mid,time,totalTrips):
+                ans.append(mid)
+                right = mid-1
+                
+            else:
+                left = mid+1
+                
+        return min(ans)
+                
+            
+            
+        
+            
+            
