6c69a97c2ebf249d83dcc8a1ea40050300d00f57
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-07-30 13:25:06 -0700

    Time: 201 ms (70.57%), Space: 14.4 MB (65.09%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key = lambda x:len(x),reverse = True)
+        dp = {word:1 for word in words}
+        
+        
+        for itr in range(len(words)-1,-1,-1):
+            temp = 1
+            for i in range(0,len(words[itr])):
+                tempW = words[itr][:i]+words[itr][i+1:]
+                word = words[itr]
+                if word in dp and tempW in dp:
+                    # print(word,dp)
+                    dp[word] = max(dp[word],1+dp[tempW])
+                
+        return max(list([v for k,v in dp.items()]))
+                
