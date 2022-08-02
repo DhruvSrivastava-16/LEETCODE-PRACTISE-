66deecfc22b4ddac2202eac035b61b38f707888c
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-08-01 22:31:44 -0700

    Time: 940 ms (52.30%), Space: 18.4 MB (8.92%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -1,39 +1,52 @@
-class Solution(object):
-    def maxNumberOfFamilies(self, n, reservedSeats):
-        """
-        :type n: int
-        :type reservedSeats: List[List[int]]
-        :rtype: int
-        """
-        dic = {}
-        for re in reservedSeats:
-            if re[0] not in dic:
-                dic[re[0]] = set()
-            dic[re[0]].add(re[1])
-        tot = n*2
-        for key, ma in dic.items():
-            a = b = c = True
-            v = 0
-            if 4 in ma or 5 in ma:
-                a=False
-                b=False
-            if 6 in ma or 7 in ma:
-                b=False
-                c=False
-            if 2 in ma or 3 in ma:
-                a=False
-            if 8 in ma or 9 in ma:
-                c=False
-            if not b:
-                if not a:
-                    v+=1
-                if not c:
-                    v+=1
-            else:
-                if not a:
-                    v+=1
-                elif not c:
-                    v+=1
-            tot-=v
-                     
-        return tot
+class Solution:
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        
+        seatMap = defaultdict(set)
+        total = 0
+        pos1 = True
+        pos2 = True
+        pos3 = True
+        
+        for seat in reservedSeats:
+            seatMap[seat[0]].add(seat[1])
+            
+        for row in seatMap.keys():
+            pos1 = True
+            pos2 = True
+            pos3 = True
+            
+            for j in range(2,6):
+                if not pos1:
+                    break
+                    
+                if j in seatMap[row]:
+                    pos1 = False
+                    
+            if pos1:
+                total+=1
+                
+            for j in range(6,10):
+                if not pos2:
+                    break
+                    
+                if j in seatMap[row]:
+                    pos2 = False
+                    
+            if pos2:
+                total+=1
+                
+            if not pos1 and not pos2:
+                
+                for j in range(4,8):
+                    if not pos3:
+                        break
+
+                    if j in seatMap[row]:
+                        pos3 = False   
+            
+                if pos3:
+                    total+=1
+            
+        rem = (n-len(seatMap.keys()))*2
+            
+        return total+rem
