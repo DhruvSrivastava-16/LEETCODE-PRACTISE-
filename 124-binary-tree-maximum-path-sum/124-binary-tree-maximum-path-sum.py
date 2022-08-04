8d3fa8d1c6d62a01654dd5b53e881988481f03fc
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-08-03 21:19:18 -0700

    Time: 90 ms (93.90%), Space: 21.3 MB (62.54%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -1,29 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        def max_gain(node):
-            global max_sum
-            if not node:
-                return 0
-
-            # max sum on the left and right sub-trees of node
-            left_gain = max(max_gain(node.left), 0)
-            right_gain = max(max_gain(node.right), 0)
-            
-            # the price to start a new path where `node` is a highest node
-            price_newpath = node.val + left_gain + right_gain
-            
-            # update max_sum if it's better to start a new path
-            max_sum = max(max_sum, price_newpath)
+    
+    def dfs(self,root):
+        if not root:
+            return 0
         
-            # for recursion :
-            # return the max gain if continue the same path
-            return node.val + max(left_gain, right_gain)
-   
-        global max_sum
-        max_sum = float('-inf')
-        max_gain(root)
-        return max_sum
+        global maxPath
+        left = max(self.dfs(root.left),0)
+        right = max(self.dfs(root.right),0)
+        maxPath = max(maxPath,root.val+left+right)
+        
+        return root.val + max(left,right)
+        
+    
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        global maxPath
+        maxPath = float('-inf')
+        self.dfs(root)
+        return maxPath
