6ebb95a8be943c0466a318399cf669bc4757a1a9
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-08-13 19:39:16 -0700

    Time: 272 ms (41.96%), Space: 17.9 MB (16.41%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,31 +1,26 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
-        m = len(word1)
-        n = len(word2)
+        dp = [[0 for i in range(len(word1)+1)] for j in range(len(word2)+1)]
         
-        if m*n == 0:
-            return m+n
-        
-        
-        dp = [[0 for i in range(m+1)] for j in range(n+1)]
-        
-        for i in range(0,m+1):
+        for i in range(len(word1)+1):
             dp[0][i] = i
             
-        for j in range(0,n+1):
+        for j in range(len(word2)+1):
             dp[j][0] = j
-            
+        
         for i in range(1,len(dp)):
             for j in range(1,len(dp[0])):
-                
+            
                 left = dp[i][j-1]
-                down = dp[i-1][j]
-                leftDown = dp[i-1][j-1]
-                if word1[j-1]!=word2[i-1]:    
-                    dp[i][j] = 1+min(left,down, leftDown)
-                    
-                else:
-                    dp[i][j]  = min(leftDown,1+min(left,down))
+                up = dp[i-1][j]
+                leftUp = dp[i-1][j-1]
+                           
+                if word1[j-1]==word2[i-1]:
                 
+                    dp[i][j] = 1+min(left,up,leftUp-1)
+                           
+                else:
+                    dp[i][j] = 1+min(left,up,leftUp)
+                           
         return dp[-1][-1]
