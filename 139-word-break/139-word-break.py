6bb032c72ad7f5037cc9d3d2c2671ddda238c186
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-08-17 10:39:40 -0700

    Time: 62 ms (49.47%), Space: 14.1 MB (25.86%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -3,24 +3,48 @@ class Solution:
         
         st = 0
         dq = deque()
-        dq.append(0)
+        seen = set()
         wordSet = set(wordDict)
-        visited = set()
-        visited.add(0)
+        dq.append(0)
+        seen.add(0)
         
-        while dq: 
-            
+        
+        while dq:
             
             st = dq.popleft()
             
             for end in range(st+1,len(s)+1):
-                temp = s[st:end]
-                if temp in wordSet and end not in visited:
-                    
-                    if end == len(s):
-                        return True
-                    
-                    dq.append(end)
-                    visited.add(end)
-            
+                if end not in seen:
+                    temp = s[st:end]
+                    if temp in wordSet:
+                        if end==len(s):
+                            return True
+                        
+                        dq.append(end)
+                        seen.add(end)
+                        
         return False
+                
+#         st = 0
+#         dq = deque()
+#         dq.append(0)
+#         wordSet = set(wordDict)
+#         visited = set()
+#         visited.add(0)
+        
+#         while dq: 
+            
+            
+#             st = dq.popleft()
+            
+#             for end in range(st+1,len(s)+1):
+#                 temp = s[st:end]
+#                 if temp in wordSet and end not in visited:
+                    
+#                     if end == len(s):
+#                         return True
+                    
+#                     dq.append(end)
+#                     visited.add(end)
+            
+#         return False
