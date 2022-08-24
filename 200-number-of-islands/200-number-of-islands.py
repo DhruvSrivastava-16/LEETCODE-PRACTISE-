13db97406998ff66ec9451e7a0e6e11ca56ffbb9
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-08-24 16:19:21 -0700

    Time: 409 ms (72.04%), Space: 16.2 MB (97.80%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,38 +1,43 @@
 from collections import deque
 
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
         dx = [-1,0,1,0]
         dy = [0,1,0,-1]
-        
         dq = deque()
+        
         count = 0
         
-        for i in range(0,len(grid)):
-            for j in range(0,len(grid[0])):
-                # print(i,j,grid)
-                if grid[i][j]=="1":
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                
+                if grid[i][j] == "1":
                     dq.append((i,j))
                     grid[i][j] = "0"
                     count+=1
                     
-                while dq:
                     
-                    x,y = dq.popleft()
                     
-                    for itr in range(4):
-                        tx = dx[itr] + x
-                        ty = dy[itr] + y
+                    while dq:
                         
-                        if tx>=0 and tx<len(grid) and ty>=0 and ty<len(grid[0]):
+                        x,y = dq.popleft()
+                        
+                        for itr in range(4):
                             
-                            if grid[tx][ty] == "1":
-                                
+                            tx = x + dx[itr]
+                            ty = y + dy[itr]
+                            
+                            if tx>=0 and ty>=0 and tx<len(grid) and ty<len(grid[0]) and grid[tx][ty] == "1":
                                 dq.append((tx,ty))
                                 grid[tx][ty] = "0"
                                 
         return count
+                            # ty = 
+                        
+                        
+                    
                                 
                     
                     
