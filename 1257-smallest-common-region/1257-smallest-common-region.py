a90473f58a393f9a2dd2cfbc17d4e04909191a73
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-08-27 13:56:21 -0700

    Time: 506 ms (12.50%), Space: 20 MB (19.88%) - LeetHub

---
+++ 1257-smallest-common-region/1257-smallest-common-region.py
@@ -0,0 +1,50 @@
+class Solution:
+    
+    def dfs(self,visited,ancestor,graph,node):
+        visited.add(node)
+        
+        for ne in graph[node]:
+            if ne not in visited:
+                ancestor[ne] = node
+                self.dfs(visited,ancestor,graph,ne)
+    
+    def findSmallestRegion(self, regions: List[List[str]], region1: str, region2: str) -> str:
+        
+        graph = defaultdict(list)
+        
+        for region in regions:
+            
+            graph[region[0]] += region[1:]
+            
+        ConnectedComponents = []
+        ancestors = []
+        visited = set()
+        itr = 0
+        for node in list(graph.keys()):
+            ancestor = {node:None}
+            
+            
+            if node not in visited:
+                itr+=1
+                print('ConnectedComponent',itr)
+                self.dfs(visited,ancestor,graph,node)
+            
+                ancestors.append(ancestor)
+            
+        ancestor = ancestors[0]
+        
+        ancestorRegion1 = set()
+        
+        Itr = region1
+        while Itr:
+            ancestorRegion1.add(Itr)
+            Itr = ancestor[Itr]
+            
+        print(ancestorRegion1)
+            
+        while True:
+            
+            if region2 in ancestorRegion1:
+                return region2
+            
+            region2 = ancestor[region2]
