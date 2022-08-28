346b200dd486cfce5f04e7a0f061975aa4719419
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-08-27 19:18:02 -0700

    Time: 456 ms (98.67%), Space: 16.7 MB (25.31%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -11,11 +11,19 @@ class Solution:
         distance = {i:float('inf') for i in range(1,n+1)}
         distance[k] = 0
         
+        visited = set()
+        # visited.add(k)
+        
         heapq.heappush(heap,(0,k))
         
         while heap:
             
             dist, node = heapq.heappop(heap)
+            
+            if node in visited:
+                continue
+                
+            visited.add(node)
             
             for ne, weight in graph[node].items():
                 
