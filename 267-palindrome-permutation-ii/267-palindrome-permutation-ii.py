99a56c47d34fa26fbc27534b1fd826e4d496536d
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-08-28 14:41:45 -0700

    Time: 257 ms (46.55%), Space: 21.4 MB (98.82%) - LeetHub

--- 267-palindrome-permutation-ii/267-palindrome-permutation-ii.py
+++ 267-palindrome-permutation-ii/267-palindrome-permutation-ii.py
@@ -7,7 +7,6 @@ class Solution:
             return 
         
         if len(s)%2!=0:
-            print("HERE")
             for key, value in counter.items():
                 
                 if value%2!=0:
@@ -19,9 +18,7 @@ class Solution:
                 else:
                     
                     if value>=2:
-                        print("2",key+temp+key)
                         counter[key]-=2
-                        # print()
                         self.backtrack(output,counter,s,key+temp+key)
                         counter[key]+=2
                         
@@ -48,31 +45,3 @@ class Solution:
         
         return output
 
-# from collections import Counter
-
-# class Solution:
-#     def generatePalindromes(self, s: str) -> List[str]:
-#         counter = Counter(s)
-#         output = []
-        
-#         def backtrack(curr='', counter=counter):
-#             if len(curr) == len(s):
-#                 output.append(curr)
-                
-# 			# Start with each letter if length is odd
-#             if not curr and len(s) % 2:
-#                 for key in counter:
-#                     counter[key] -= 1
-#                     backtrack(key, counter)
-#                     counter[key] += 1
-					
-#             # Build palindrome around current string  
-#             else:
-#                 for key, count in counter.items():
-#                     if count >= 2:
-#                         counter[key] -= 2
-#                         backtrack(key + curr + key, counter)
-#                         counter[key] += 2
-        
-#         backtrack()
-#         return output
