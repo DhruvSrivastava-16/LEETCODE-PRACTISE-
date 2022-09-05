9b089991d9bbde709951a6b98f49aa9c5593a687
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-09-04 18:32:03 -0700

    Time: 789 ms (77.06%), Space: 28.9 MB (95.82%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,30 +1,32 @@
 class Solution:
-    def isPali(self,word):
-        rev = word[::-1]
+    
+    def isRev(self,word):
         
-        if word==rev:
+        if word==word[::-1]:
             return True
         
         return False
     
     def dfs(self,word,pos,answer,temp):
-        if pos==len(word):
+        # print(temp)
+        if pos == len(word):
             answer.append(temp[:])
-            return
+            return 
         
         for itr in range(pos+1,len(word)+1):
-            if self.isPali(word[pos:itr]):
+            
+            if self.isRev(word[pos:itr]):
                 temp.append(word[pos:itr])
                 self.dfs(word,itr,answer,temp)
                 temp.pop()
                 
                 
-            
     
     def partition(self, s: str) -> List[List[str]]:
+        
         
         
         answer = []
         temp = []
         self.dfs(s,0,answer,temp)
-        return answer
+        return(answer)
