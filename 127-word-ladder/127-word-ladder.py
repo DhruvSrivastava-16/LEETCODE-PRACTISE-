774d5e46da22147597ef117c07d15f63d2085010
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-09-05 17:21:45 -0700

    Time: 228 ms (65.91%), Space: 19.7 MB (9.64%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,38 +1,35 @@
-from collections import defaultdict, deque
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         
-        hmap = defaultdict(list)
         
+        hmap = defaultdict(set)
         for word in wordList:
-            for i in range(len(word)):
-                temp = word[:i] + "*" + word[i+1:]
-                hmap[temp].append(word)
+            for loc in range(len(word)):
+                
+                temp = word[:loc]+"*"+word[loc+1:]
+                hmap[temp].add(word)
                 
         dq = deque()
         dq.append((beginWord,1))
         visited = set()
         visited.add(beginWord)
-        step = 0
         
         while dq:
             
             word, step = dq.popleft()
             
-            
-            for i in range(len(beginWord)):
-                temp = word[:i]+'*'+word[i+1:]
+            for loc in range(len(word)):
                 
-                for wrd in hmap[temp]:
-                    
-                    if wrd == endWord:
+                temp = word[:loc] + "*" + word[loc+1:]
+
+                
+                for word2 in hmap[temp]:
+                    if word2==endWord:
                         return step+1
                     
-                    if wrd not in visited:
-                        visited.add(wrd)
-                        dq.append((wrd,step+1))
-            
+                    if word2 not in visited:
+                        visited.add(word2)
+                        dq.append((word2,step+1))
+                    
         return 0
-            
-            
+                
