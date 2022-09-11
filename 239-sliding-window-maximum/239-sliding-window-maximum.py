79a07caed8d9ab9dfa573efec0947fb7f3826daa
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-09-11 13:28:34 -0700

    Time: 4489 ms (7.72%), Space: 30 MB (46.58%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,16 +1,33 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         from collections import deque
-        q = deque() # stores *indices*
+        dq = deque() # stores *indices*
         res = []
-        for i, cur in enumerate(nums):
-            while q and nums[q[-1]] <= cur:
-                q.pop()
-            q.append(i)
-            # remove first element if it's outside the window
-            if q[0] == i - k:
-                q.popleft()
-            # if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
-            if i >= k - 1:
-                res.append(nums[q[0]])
+        for index, value in enumerate(nums):
+            
+
+
+            while dq and value>=nums[dq[-1]]:
+                dq.pop() 
+
+            dq.append(index)
+
+            if dq[0]==index-k:
+                dq.popleft()
+
+            if index>=k-1:
+
+                res.append(nums[dq[0]])
+                    
         return res
+        # for i, cur in enumerate(nums):
+        #     while q and nums[q[-1]] <= cur:
+        #         q.pop()
+        #     q.append(i)
+        #     # remove first element if it's outside the window
+        #     if q[0] == i - k:
+        #         q.popleft()
+        #     # if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
+        #     if i >= k - 1:
+        #         res.append(nums[q[0]])
+        # return res
