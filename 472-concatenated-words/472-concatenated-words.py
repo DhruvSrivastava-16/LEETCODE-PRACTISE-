d7ef9973b459b72c7ef3f1e0d8225d3f069217bd
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-09-24 00:34:43 -0700

    Time: 632 ms (68.97%), Space: 16.7 MB (85.20%) - LeetHub

---
+++ 472-concatenated-words/472-concatenated-words.py
@@ -0,0 +1,31 @@
+class Solution:
+    
+    def dfs(self,loc,word,count, wordSet):
+        if loc>=len(word):
+            if count>1:
+                return True
+            
+            else:
+                return False
+            
+        for idx in range(loc,len(word)):
+            
+            if word[loc:idx+1] in wordSet and self.dfs(idx+1,word,count+1,wordSet):
+                return True
+            
+            
+        return False
+            
+        
+    
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        wordSet = set(words)
+        answer = []
+        
+        
+        for i in range(len(words)):
+            count = 0
+            if self.dfs(0,words[i],count,wordSet):
+                answer.append(words[i])
+                
+        return(answer)
