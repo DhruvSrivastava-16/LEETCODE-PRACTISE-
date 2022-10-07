ed476672cfedc9ae57c336c46c4cc465066020db
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-10-07 15:58:54 -0700

    Time: 1355 ms (39.63%), Space: 31.5 MB (5.79%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,33 +1,40 @@
 class Solution:
     
-    def helper(self,heights,idx,bricks,ladders):
+    def helper(self,heights,idx,bricks,ladders,climbs):
         
-        climbs = []
-        for i in range(1,len(heights[:idx+1])):
-            if heights[i]-heights[i-1]>0:
-                climbs.append(heights[i]-heights[i-1])
-                
-        climbs.sort()
+
         
         bricks_remaining = bricks
         ladders_remaining = ladders
-        for climb in climbs:
+        for climb2 in climbs:
             # If there are enough bricks left, use those.
-            if climb <= bricks_remaining:
-                bricks_remaining -= climb
-            # Otherwise, you'll have to use a ladder.
-            elif ladders_remaining >= 1:
-                ladders_remaining -= 1
-            # And if there are no ladders either, we can't reach buildingIndex.
-            else:
-                return False
+            climb = climb2[0]
+            cond = climb2[1]
+            
+            if cond<=idx:
+                if climb <= bricks_remaining:
+                    bricks_remaining -= climb
+                # Otherwise, you'll have to use a ladder.
+                elif ladders_remaining >= 1:
+                    ladders_remaining -= 1
+                # And if there are no ladders either, we can't reach buildingIndex.
+                else:
+                    return False
         return True
+    
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        
+        climbs = []
+        for i in range(1,len(heights)):
+            if heights[i]-heights[i-1]>0:
+                climbs.append((heights[i]-heights[i-1],i))
+                
+        climbs.sort()
         lo = 0
         hi = len(heights) - 1
         while lo < hi:
             mid = lo + (hi - lo + 1) // 2
-            if self.helper(heights,mid,bricks,ladders):
+            if self.helper(heights,mid,bricks,ladders,climbs):
                 lo = mid
             else:
                 hi = mid - 1
