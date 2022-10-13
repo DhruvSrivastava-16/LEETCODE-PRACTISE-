1974e2d5ef175a0c3be39f460b464e8558f31acf
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-10-13 16:59:14 -0700

    Time: 54 ms (48.55%), Space: 13.9 MB (81.68%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,29 +1,29 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        # quick check on the characters,
-        #   otherwise it would exceed the time limit for certain test cases.
         if set(Counter(s).keys()) > set(Counter("".join(wordDict)).keys()):
             return []
         
         wordSet = set(wordDict)
         
         dp = [[]]*(len(s)+1)
-        dp[0] = [""]
+        dp[0] = " "
         
         for end in range(1,len(s)+1):
             
-            sub = []
+            temp = []
             
             for st in range(0,end):
                 
-                word = s[st:end]
+                wrd = s[st:end]
                 
-                if word in wordSet:
+                if wrd in wordSet:
                     
                     for sq in dp[st]:
-                        
-                        sub.append((sq+' '+word).strip())
-                        
-            dp[end] = sub
-            print(dp,sub)
+                    
+                        temp.append((sq+" "+wrd).strip())
+                    
+            dp[end] = temp
+            
         return dp[-1]
+                
+            
