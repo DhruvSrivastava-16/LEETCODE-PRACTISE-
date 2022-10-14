a22ef5d37b9c8250d4fc462d10201a809fdc4865
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2022-10-13 17:14:59 -0700

    Time: 8439 ms (5.04%), Space: 13.8 MB (78.51%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -3,34 +3,22 @@ class Solution:
         global count 
         count = 0
         temp = []
-        visited = set()
-        self.bt(1,n,temp,visited)
+        idx = 1
+        self.bt(temp,n,idx)
         return count
         
-    def bt(self,idx,n,temp,visited):
+    def bt(self,temp,n,idx):
         global count
-        if len(temp)==n:
+        if len(temp) == n:
             count+=1
             return 
         
         for i in range(1,n+1):
             if i not in temp and (i%idx==0 or idx%i==0):
                 temp.append(i)
-                self.bt(idx+1,n,temp,visited)
+                idx+=1
+                self.bt(temp,n,idx)
                 temp.pop()
+                idx-=1
                 
-#     def countArrangement(self, n: int) -> int:
-#         self.count = 0
-#         self.backtrack(n, 1, [])
-#         return self.count
-        
-#     def backtrack(self, N, idx, temp):
-#         if len(temp) == N:
-#             self.count += 1
-#             return
-        
-#         for i in range(1, N+1):
-#             if i not in temp and (i % idx == 0 or idx % i == 0):
-#                 temp.append(i)
-#                 self.backtrack(N, idx+1, temp)
-#                 temp.pop()
+            
